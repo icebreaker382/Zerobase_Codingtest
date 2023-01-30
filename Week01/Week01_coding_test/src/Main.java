92a226b94db68b0e8115113338fd9791d9c19c24
icebreaker382 <76737614+icebreaker382@users.noreply.github.com>
2023-01-30 10:03:46 -0500

    Delete Week01 directory

--- Week01/Week01_coding_test/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int n = 99;
-        System.out.println("5%%2 = "+5%2);
-        Solution sol = new Solution();
-        int answer = sol.solution(n);
-        System.out.println("answer = " + answer);
-    }
-}
--- Week01/Week01_coding_test3/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int num = 4;
-        int ans = sol.solution(num);
-        System.out.println("결과 = " + ans);
-    }
-}
