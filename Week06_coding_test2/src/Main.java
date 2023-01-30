9533f2144b41081b50b0725c753abd6060112fb8
icebreaker382 <76737614+icebreaker382@users.noreply.github.com>
2023-01-30 10:07:43 -0500

    Delete Week06_coding_test2 directory

--- Week06_coding_test2/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int[] nums = {3, 30, 34, 5, 9};
-        Solution sol = new Solution();
-        System.out.println(sol.solution(nums));
-    }
-}
